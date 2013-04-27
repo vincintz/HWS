2b453cd305af62f193312694aba8776463023496
Theo Reyes <vincintz@yahoo.com>
2013-04-28 02:26:35 +0800

    initial commit

---
+++ com.samples.hws.basic/src/main/java/com/samples/first/App.java
@@ -0,0 +1,13 @@
+package com.samples.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
