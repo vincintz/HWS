28fbb6cbf79df564f63885201128b77d33d4b6f4
Theo Reyes <vincintz@yahoo.com>
2013-05-05 18:51:01 +0800

    Converted TimeService to PingService

--- com.samples.hws.basic/src/main/java/com/samples/hws/basic/service/PingService.java
+++ com.samples.hws.basic/src/main/java/com/samples/hws/basic/service/PingService.java
@@ -2,6 +2,7 @@ package com.samples.hws.basic.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import org.springframework.format.datetime.DateFormatter;
@@ -10,10 +11,22 @@ import org.springframework.stereotype.Service;
 import java.util.Calendar;
 import java.util.Locale;
 
-@Service("timeService")
-@Path("/time")
-public class TimeService {
+@Service("pingService")
+@Path("/ping")
+public class PingService {
     @GET
+    @Produces("text/plain")
+    public String ping() {
+        return "pong";
+    }
+    @GET
+    @Path("/{input}")
+    @Produces("text/plain")
+    public String echo(@PathParam("input") final String input) {
+        return "pong - " + input;
+    }
+    @GET
+    @Path("/time")
     @Produces("text/plain")
     public String getDateTime() {
         final DateFormatter formatter = new DateFormatter("dd/MM/yyyy hh:mm:ss");
